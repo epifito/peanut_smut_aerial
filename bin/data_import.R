ef8e72373dd4daaf990745ed25c3a1c0d6a6b255
juanchiem <edwardsmolina@gmail.com>
2021-09-21 11:00:27 -0300

    juan work

--- bin/data_import.R
+++ bin/data_import.R
@@ -3,9 +3,9 @@ pacman::p_load(tidyverse, googlesheets4)
 gs4_auth()
 url <- "https://docs.google.com/spreadsheets/d/1fBHWS1qCVNoj3yn0VcK9-598CS-Q95esJx8SXvPd5Uo/edit?usp=sharing"
 smut <- gs4_get(url)
-# gs4_browse(smut)
+gs4_browse(smut)
 
-dat <- data_frame(field = smut %>% sheet_names()) %>% 
+dat <- data_frame(field = sheet_names(smut) %>% str_subset(pattern = "^field")) %>% 
   mutate(data = map(field, ~read_sheet(smut, sheet = .x))) %>% 
   tidyr::unnest(cols = c(data))  %>% 
   mutate_if(is.character, as.factor)
--- bin/wind.R
+++ bin/wind.R
@@ -29,14 +29,39 @@ meteo_dat %>%
 
 meteo_dat %>% 
   ggplot(aes(x=W_DIR, fill=W_DIR))+ 
-  geom_bar(stat="count",width=1,colour="black",size=0.1, alpha= 0.5)+ 
+  geom_bar(stat="count", width=1, colour="black", size=0.1, alpha= 0.5)+ 
   coord_polar(theta = "x", start=6.0729, direction = 1)+
   scale_color_discrete ()+ 
   labs(x="", y="", title = "Frequency wind rose")+ 
   scale_fill_discrete(drop=FALSE) + scale_x_discrete(drop=FALSE)+
   guides(fill="none") +
+  facet_wrap("FIELD")+
+  # theme_void()
   theme(axis.text.y=element_blank(),
-        axis.ticks.y = element_blank()) +
-  facet_wrap("FIELD")
+        axis.ticks.y = element_blank())
 
 ggsave(last_plot(), file = "plots/wind_rose.jpg", width = 15, height = 10, units = "cm",  dpi = 600)
+
+dir <- tibble(degrees= seq(0, 337.5 , by=22.5), 
+              W_DIR= c("N","NNE","NE", "ENE", "E", "ESE", "SE", "SSE", 
+                       "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"))
+meteo <- meteo_dat %>% left_join(dir)  
+
+pw <-
+  with(
+    meteo,
+    windrose(
+      speed=W,
+      direction=degrees,
+      as.factor(FIELD), 
+      n_col = 3, 
+      n_speeds = 5
+      )
+  )
+pw +
+  scale_fill_viridis_d(name = "Wind Speed (m/s)", direction = -1) +
+  xlab("") +
+  cowplot::theme_minimal_grid()
+meteo  
+
+
