4c6e9b852f6a9a1cb7f3e32e14a5ffec7f82c5cd
Adam Sparks <adamhsparks@gmail.com>
2021-11-13 08:53:25 +0800

    Use Tweedie family, greatly improves model fit

--- brms_modelling.R
+++ brms_modelling.R
@@ -1,21 +1,62 @@
 library(brms)
 library(bayesplot)
 library(bayestestR)
+library(here)
+library(parallel)
+
+options(mc.cores = detectCores())
 
 load(here("data/dat.Rdata"))
 load(here("data/mod_dat.Rdata"))
 
+priors <- prior_string("cauchy(0, 10)")
+
 bm1 <-
   brm(
-    formula = spore_cm2 ~  downwind + (1 | field) ,
+    formula = spore_cm2 ~ downwind + (1 | field) ,
     data = mod_dat,
     seed = 27,
     prior = priors,
     family = "hurdle_gamma",
-    iter = 3000,
+    control = list(adapt_delta = 0.999,
+                   max_treedepth = 15),
+    iter = 1000,
     chains = 4
   )
 
-pp_chec(bm1)
+pp_check(bm1)
 
-plot(pd(m1))
+plot(pd(bm1))
+
+
+bm2 <-
+  brm(
+    formula = spore_cm2 ~ distance_m + (1 | field) ,
+    data = mod_dat,
+    seed = 27,
+    prior = priors,
+    family = "hurdle_gamma",
+    chains = 4
+  )
+
+pp_check(bm2)
+
+plot(pd(bm2))
+
+bm3 <-
+  brm(
+    formula = spore_cm2 ~ time_min + (1 | field),
+    data = mod_dat,
+    seed = 27,
+    prior = priors,
+    family = "hurdle_gamma",
+    control = list(adapt_delta = 0.999,
+                   max_treedepth = 15),
+    iter = 2000,
+    chains = 4
+  )
+
+pp_check(bm3)
+
+plot(pd(bm3))
+
